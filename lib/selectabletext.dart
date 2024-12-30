fb73de02c89aa25bf6dd0c54280bace211273855
darwynkjoy <darwynkjoy@gmail.com>
2024-12-30 13:26:28 +0530

    daily push

---
+++ lib/selectabletext.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SelectabletextExample extends StatefulWidget{
+  @override
+  State<SelectabletextExample> createState()=> _SelectabletextState();
+}
+class _SelectabletextState extends State<SelectabletextExample>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Selectable Text Example"),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Column(
+        children: [
+          Center(child: Text('''this cannot be selected because its not a SELECTABLETEXT''',style: TextStyle(fontSize: 15),)),
+          Center(child: Text('''this can be selected because its  a SELECTABLETEXT''',style: TextStyle(fontSize: 15),)),
+        ],
+      ),
+    );
+  }
+}
